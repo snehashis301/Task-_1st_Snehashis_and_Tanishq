4d3c94800048075dfc1710df0c081de95fef4c29
snehashis301 <80242201+snehashis301@users.noreply.github.com>
2021-03-08 16:34:59 +0530

    Add files via upload

---
+++ 3rd_prog.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class DisplayOperations extends Operations
+{
+    public void showAttributes()
+	{
+        System.out.prinltn(this.id);
+        System.out.prinltn(this.name);
+        System.out.prinltn(this.percentage);
+        System.out.prinltn(this.skills);
+    }
+    public void showNAME()
+	{
+        System.out.prinltn(this.name.toUpperCase());
+    }
+    public void showDifference(Student s2)
+	{
+        System.out.prinltn(this.percentage-s2.percentage);
+    }
+}
+
